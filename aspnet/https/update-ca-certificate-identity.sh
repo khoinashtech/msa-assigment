2c5694b64652fd19e1d854feb4ecf1956f740683
khoilen <khoilen@nashtechglobal.com>
2023-10-27 22:55:23 +0700

    add service bank

--- aspnet/https/update-ca-certificate-identity.sh
+++ aspnet/https/update-ca-certificate-identity.sh
@@ -4,5 +4,4 @@ echo "startup script is running"
 
 cp /app/https/localhost.crt /usr/local/share/ca-certificates
 update-ca-certificates
-
-dotnet MSA.ProductService.dll
+dotnet MSA.IdentityServer.dll
